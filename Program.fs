dba73a53510900b858c866e3752a2e063f9bdfa5
Кристина Валенская <masterkristall@gmail.com>
2019-11-19 10:01:01 +0700

    1

--- Program.fs
+++ Program.fs
@@ -9,7 +9,7 @@ open Sorting
 let main argv = 
 
     let a = new Algo()
-    let arr = [|7; 9; 34; 21; 90|]
-    let res = a.BinaryInsertion(arr)
+    //let arr = [|7; 9; 34; 21; 90|]
+    //let res = a.BinaryInsertion(arr)
     Console.ReadKey() |> ignore
     0 // возвращение целочисленного кода выхода
